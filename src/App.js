a5a4e031784e27b53cf65b13bacbd29077844fd5
matthew <larochelle.matthew@gmail.com>
2023-05-25 11:48:54 -0700

    Pause in coding at video #4

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar';
 import Booklist from './components/Booklist';
 import ThemeContextProvider from './contexts/ThemeContext';
 
+// I stopped coding after doing 3 videos (start at #4 if needed)
 
 function App() {
   return (
